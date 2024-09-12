8bca6f01d52a3f9cc31092841ce364f6afa5de1c
alifahmed76 <alifahmed.cse76@gmail.com>
2024-09-13 01:20:53 +0600

    project finalize stage

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>{{ __('common.Version') }}</b> 1.0.1
+    </div>
+    <strong>{{ __('common.Copyright') }} &copy; 2024-2026 <a>Alif Ahmed</a>.</strong>
+    {{ __('common.all_reserved') }}
+</footer>
